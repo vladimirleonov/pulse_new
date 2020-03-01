2ec867bb98702cea7866ce01ee91fabf5256d7f1
vova <vladimirleonov789@gmail.com>
2020-03-01 15:46:23 +0300

    finish first screen

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,14 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function(){
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({
                 prefix: "",
                 suffix: ".min",
               }))
             .pipe(autoprefixer({
-                cascade: false
+                cascade: false 
             }))
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
@@ -30,7 +30,7 @@ gulp.task('styles', function(){
 })
 
 gulp.task('watch', function(){
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
