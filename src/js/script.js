8bedf8c84ded6b2a4366359999ef0d5203305b75
vladimir <vladimirleonov789@gmail.com>
2020-03-28 13:27:19 +0300

    finished making up the slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         infinite: false,
         speed: 900,
         draggable: false, //отключить листание мышкой слайдов на desktop
         //swipe: false, //отключить листание мышкой слайдов на тачскрин
         //touchTHreshold: 5, //по умолчанию 5 //для того чтобы сработал слайд нужно просвайпить меньшее расстояние при увеличениии значения нужно сделать меньшее усилие и наоборот
-        //touchMove:false //при перетаскивании пальцем мы не увидим перемещения но когда прошол это расстояние то переключится touchTHreshold:5
+        //touchMove: false //при перетаскивании пальцем мы не увидим перемещения но когда прошол это расстояние то переключится touchTHreshold:5
         //adaptiveHeight: true,
+        waitForAnimate: false, //по умолчанию включена // на сколько быстро пользователь будет нажимать на стрелку на столько быстро будет перелистываться
+        //ceterMode: true, //когда показывается несколько слайдов, то один будет строго по центру 
+        //rows: 3, //3 ряда
+        //slidesPerRow: 2 //количество слайдов в ряду
+        // vertical: true //вертикальный слайдер
+        // verticalSwiping: true //вертикальная прокрутка
+        // fade: true // красивое слайд-шоу, не листаются
+        // asNavFor:".slider" //указываем слайдер с которым связыаемся(можно сделать эффект увеличения, т.е. листая нижний на верхнем те же картинки в увеличеном виде)
+        // appendArrows:$(.content)// поместить стрелки в класс .content
+        // appendDots:$(.content)// поместить  точки в класс .content
         slidesToShow: 1,
-        slidesToScroll: 1, 
+        slidesToScroll: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/left-arrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/right-arrow.png"></button>',
         responsive: [
@@ -21,4 +31,4 @@ $(document).ready(function(){
             }
         ]
     });
-  });
+});
