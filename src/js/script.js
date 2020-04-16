33137938361e490b32140a1f38f337ae8b53decb
Vladimir <vladimirleonov789@gmail.com>
2020-04-16 20:33:18 +0300

    add scroll

--- src/js/script.js
+++ src/js/script.js
@@ -124,6 +124,24 @@ $(document).ready(function () {
         return false;
     })
 
+    //scroll fadeIn or fadeOut
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    //scroll smooth 
+    $(function () {
+        $("a[href^='#']").click(function () {
+            var _href = $(this).attr("href");
+            $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+            return false;
+        });
+    });
+
 });
 
 
