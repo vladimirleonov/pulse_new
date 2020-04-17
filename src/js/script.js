9b286683c71e7b91940a9bb4706de3722c3e7fac
Vladimir <vladimirleonov789@gmail.com>
2020-04-17 19:37:00 +0300

    correct some moment

--- src/js/script.js
+++ src/js/script.js
@@ -135,7 +135,7 @@ $(document).ready(function () {
     });
     //scroll smooth top 
     $(function () {
-        $("a[href^='#']").click(function () {
+        $("a[href^=#up]").click(function () {
             var _href = $(this).attr("href");
             $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
             return false;
