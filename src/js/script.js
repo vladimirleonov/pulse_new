564214c47b7abf3dbebaf3fca289fa18e4a2ae88
Vladimir <vladimirleonov789@gmail.com>
2020-04-18 14:51:15 +0300

    fixed validation errors of the website

--- src/js/script.js
+++ src/js/script.js
@@ -135,7 +135,7 @@ $(document).ready(function () {
     });
     //scroll smooth top 
     $(function () {
-        $("a[href^=#up]").click(function () {
+        $("a[href^=#]").click(function () {
             var _href = $(this).attr("href");
             $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
             return false;
